2a0791faaccd582ce2b6d1d11635ed006d2c608c
AcipenserSturio <AcipenserSturio@users.noreply.github.com>
2025-05-03 08:51:06 +0500

    add dark mode

---
+++ public/light-mode.js
@@ -0,0 +1,30 @@
+// This is done as a separate public file,
+// so that I can put it as a loaded <script> in head.
+// there may be better ways? this one works.
+
+const theme = (() => {
+  const localStorageTheme = localStorage?.getItem("theme") ?? "";
+  if (["dark", "light"].includes(localStorageTheme)) {
+    return localStorageTheme;
+  }
+  if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+    return "dark";
+  }
+  return "light";
+})();
+
+if (theme === "light") {
+  document.documentElement.classList.remove("dark");
+} else {
+  document.documentElement.classList.add("dark");
+}
+
+window.localStorage.setItem("theme", theme);
+
+const handleToggleClick = () => {
+  const element = document.documentElement;
+  element.classList.toggle("dark");
+
+  const isDark = element.classList.contains("dark");
+  localStorage.setItem("theme", isDark ? "dark" : "light");
+};
