0dd44e2fdab3fcd95751b8fd435572bc228426f0
AcipenserSturio <AcipenserSturio@users.noreply.github.com>
2025-05-25 16:52:10 +0500

    fix: remove ! from exercises

--- src/utils/exercise.ts
+++ src/utils/exercise.ts
@@ -5,7 +5,7 @@ export interface Exercise {
 }
 
 export const tokeniseSentence = (sentence: string) =>
-  sentence.split(" ").map((w) => w.replaceAll(/[\.\,\?\:]/g, "").trim());
+  sentence.split(" ").map((w) => w.replaceAll(/[\.\,\?\:\!]/g, "").trim());
 
 export const verifyChips = (exercise: Exercise, chips: string[]) =>
   (exercise.alsoAccept ? [exercise.l2, ...exercise.alsoAccept] : [exercise.l2])
