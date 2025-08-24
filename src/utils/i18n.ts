78a33a0f713f7780adebd68a1030019fbb7e62bf
AcipenserSturio <AcipenserSturio@users.noreply.github.com>
2025-08-25 01:28:51 +0500

    fixes

--- src/utils/i18n.ts
+++ src/utils/i18n.ts
@@ -5,6 +5,7 @@ export const langNames: Record<string, string> = {
   en: "English",
   de: "Deutsch",
   pl: "polski",
+  pt: "Português",
   ru: "русский",
   he: "עברית",
   translate: "add language",
