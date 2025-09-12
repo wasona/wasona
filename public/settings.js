232e8e0d24842f2b8a4e2219ba0401a83e4dc664
Gingeh <39150378+Gingeh@users.noreply.github.com>
2025-09-12 16:04:17 +1000

    feat: toggle to hide answers

--- public/settings.js
+++ public/settings.js
@@ -21,10 +21,34 @@ if (theme === "light") {
 
 window.localStorage.setItem("theme", theme);
 
-const handleToggleClick = () => {
+const handleThemeToggleClick = () => {
   const element = document.documentElement;
   element.classList.toggle("dark");
 
   const isDark = element.classList.contains("dark");
   localStorage.setItem("theme", isDark ? "dark" : "light");
+};
+
+const shouldHideAnswers = (() => {
+  const localStorageTheme = localStorage?.getItem("hide-answers") ?? "";
+  if (["yes", "no"].includes(localStorageTheme)) {
+    return localStorageTheme;
+  }
+  return "no";
+})();
+
+if (shouldHideAnswers === "no") {
+  document.documentElement.classList.remove("hide-answers");
+} else {
+  document.documentElement.classList.add("hide-answers");
+}
+
+window.localStorage.setItem("hide-answers", shouldHideAnswers);
+
+const handleHideAnswersToggleClick = () => {
+  const element = document.documentElement;
+  element.classList.toggle("hide-answers");
+
+  const shouldHideAnswers = element.classList.contains("hide-answers") ? "yes" : "no";
+  localStorage.setItem("hide-answers", shouldHideAnswers);
 };
