7ba458fe86fca13c01baec1ae32b67f01141c7f7
AcipenserSturio <AcipenserSturio@users.noreply.github.com>
2025-10-10 16:00:50 +0500

    style

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,23 +2,20 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
 import react from "@astrojs/react";
 
 const deploy = import.meta.env.PROD
   ? { site: "https://wasona.com/" }
   : { site: "http://localhost/" };
 
-const redirects = {
-  "/en/": {
-    status: 301,
-    destination: "/",
-  }
-}
-
 // https://astro.build/config
 export default defineConfig({
   ...deploy,
   integrations: [mdx(), sitemap(), react()],
-  redirects,
+  redirects: {
+    "/en/": {
+      status: 301,
+      destination: "/",
+    },
+  },
 });
