d868241b3aa7481e2b0d62bd67b2eb93ed72b669
AcipenserSturio <AcipenserSturio@users.noreply.github.com>
2025-11-30 21:14:57 +0300

    chore: cleanup

--- src/lib/exercise.ts
+++ src/lib/exercise.ts
@@ -22,7 +22,7 @@ export const tokeniseSentence = (sentence: string) =>
   sentence.replaceAll(/[\.\,\?\:\!]$/g, "").split(" ");
 // .map((w) => w.trim());
 
-export const verifyChips = (exercise: Task, chips: string[]) =>
-  (exercise.alsoAccept ? [exercise.l2, ...exercise.alsoAccept] : [exercise.l2])
+export const verifyChips = (task: Task, chips: string[]) =>
+  (task.alsoAccept ? [task.l2, ...task.alsoAccept] : [task.l2])
     .map((sentence) => tokeniseSentence(sentence).join(" "))
     .includes(chips.join(" "));
