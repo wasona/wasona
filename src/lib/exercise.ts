fc3d37dbeb4e293a1bb43a2179c4498f90021f5e
AcipenserSturio <AcipenserSturio@users.noreply.github.com>
2025-11-30 13:24:25 +0300

    schema: lesson->exercise->task

--- src/lib/exercise.ts
+++ src/lib/exercise.ts
@@ -1,15 +1,28 @@
-export interface Exercise {
-  l1: string;
-  l2: string;
-  alsoAccept?: string[];
-  junkChips?: string[];
-}
+import { z } from "zod";
+
+export const task = z
+  .object({
+    l1: z.string(),
+    l2: z.string(),
+    alsoAccept: z.string().array().optional(),
+    junkChips: z.string().array().optional(),
+  })
+  .strict();
+export type Task = z.infer<typeof task>;
+
+export const exercise = z
+  .object({
+    title: z.string(),
+    tasks: z.array(task),
+  })
+  .strict();
+export type Exercise = z.infer<typeof exercise>;
 
 export const tokeniseSentence = (sentence: string) =>
   sentence.replaceAll(/[\.\,\?\:\!]$/g, "").split(" ");
 // .map((w) => w.trim());
 
-export const verifyChips = (exercise: Exercise, chips: string[]) =>
+export const verifyChips = (exercise: Task, chips: string[]) =>
   (exercise.alsoAccept ? [exercise.l2, ...exercise.alsoAccept] : [exercise.l2])
     .map((sentence) => tokeniseSentence(sentence).join(" "))
     .includes(chips.join(" "));
