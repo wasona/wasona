7dba55e4602fcec0388371cf8b0a0f5cdb9c8b0a
tbodt <tbodt@tbodt.com>
2026-09-03 02:20:35 -0700

    o jo pona e sona tan insa lipu pi nasin json kepeken nasin import (#135)
    ilo import li kama e lipu pi nasin json la ona li pana taso ala e ona,
    li pana e ijo module. tan ona la mi ken jo e ijo JSON lon kepeken nimi
    'default'. ilo li ike

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -4,5 +4,5 @@ import { toCodes } from "astro:i18n";
 export const langs = toCodes(i18n!.locales);
 export const locales: Record<string, any> = {};
 for (let lang of langs) {
-  locales[lang] = await import(`../../content/${lang}/locale.json`);
+  locales[lang] = (await import(`../../content/${lang}/locale.json`)).default;
 }
